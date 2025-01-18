489a0ebab9e3b8ca24bfa697eaf883fdb06f1241
DeveshGarkoti <deveshgar280@gmail.com>
2025-01-18 09:40:51 +0530

    postgress_aws

--- chaiheadq/settings.py
+++ chaiheadq/settings.py
@@ -76,13 +76,18 @@ WSGI_APPLICATION = 'chaiheadq.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'BackendCourse',
+        'USER': 'mysuperuser',
+        'PASSWORD': 'mysuperuser',
+        'HOST': 'backendcourse.cfo82g2wymzv.eu-north-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
